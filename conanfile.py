cebfccc1c25888cb492be92901930544df9f369f
Jeremy D. Gola <jeremy@alti-2.com>
2020-03-18 16:08:43 -0400

    Changed conan file to reflect current methods.

--- conanfile.py
+++ conanfile.py
@@ -5,74 +5,39 @@ class xxHash(ConanFile):
     name = "xxhash"
     license = "BSD-2-Clause"
     version = "v0.6.5"
-    settings = "compiler", "arch", "target"
-    generators = "cmake"
     scm = {
         "type": "git",
         "url": "https://github.com/Alti-2/xxHash.git",
         "revision": "conan_cmake"
     }
-    options = {"long_long_support": [True, False], "bundled_mode": [True, False]}
-    default_options = {"long_long_support": False, "bundled_mode": True}
+    options = {"long_long_support": [True, False], "inline_all": [True, False], "shared": [True, False], 
+               "bundled_mode": [True, False]}
+    default_options = {"long_long_support": False, "inline_all": False, "shared": False, 
+                       "bundled_mode": True}
     generators = "cmake", "compiler_args", "visual_studio"
     build_requires = "ninja/[>=1.9.0]", "cmake/[>=3.1.2]", "unity/[>=2.5.0]",
     python_requires = "SES-ARM/[>=4]", "conan-helper/[>=0.0.1]",
     python_requires_extend = "conan-helper.Importer", "conan-helper.DependencyManager", "conan-helper.ConfigurationManager", "conan-helper.ArchiveHelper"
     _source_list = ["xxhash.c"]
     _header_list = ["xxhash.h"]
-    _compile_defines = {"XXH_NO_LONG_LONG": 1, }
+    _compile_defines = {"XXH_BUNDLED_MODE": None}
+    _vs_sln = os.path.join("visual_studio", "xxHash.sln")
+
+    def _set_compile_defines(self):
+        if self.options.inline_all == True:
+            self._compile_defines["XXH_INLINE_ALL"] = 1
+
+        if self.options.long_long_support == False:
+            self._compile_defines["XXH_NO_LONG_LONG"] = 1
+
+        self._compile_defines["XXH_BUNDLED_MODE"] = int(self.options.bundled_mode == True)
 
     def build(self):
-        cmake = CMake(self)
-        compile_defines = {"XXH_NO_LONG_LONG": 1, }
-            
-        # SES doesn't play nice with CMake without major modifications.
-        if self.settings.compiler == "ses_arm":
-            self.python_requires["SES-ARM"].module.ses_arm.build_archive(self, "xxhash.c", compile_defines)
-        # In all other cases, use CMake.
-        else:
-            if self.should_configure:
-                cmake.verbose = True
-                
-                if self.settings.compiler != "Visual Studio":
-                    print("Using ninja.")
-                    cmake.definitions["CMAKE_MAKE_PROGRAM"] = "ninja"
+        self._set_compile_defines()
 
-                cmake_defines = None
-
-                if self.settings.target == "mcu":
-                    # xxHash supports "bundled mode", which does not install the final product on the host machine.
-                    # If we're cross compiling, we definitely do not want to install.
-                    cmake.definitions["XXHASH_BUNDLED_MODE"] = "ON"
-                    # Cross compiling to an mcu means the xxhsum project is not compatible.
-                    cmake_defines = dict({"BUILD_XXHSUM": 0, "BUILD_SHARED_LIBS": 0, "XXH_NO_LONG_LONG": 1})
-                    cmake.definitions["CMAKE_C_ABI_COMPILED"] = "arm-none-eabi"
-                    cmake.definitions["CMAKE_CROSS_COMPILING"] = 1
-                    cmake.definitions["CONAN_DISABLE_CHECK_COMPILER"] = 1
-                    
-                    if str(self.settings.arch).find("arm") != -1:
-                        cmake.definitions["CMAKE_SYSTEM_PROCESSOR"] = "arm"
-                
-                cmake.definitions["XXHASH_BUNDLED_MODE"] = "ON"
-                # Cross compiling to an mcu means the xxhsum project is not compatible.
-                cmake_defines = dict({"BUILD_XXHSUM": 0, "BUILD_SHARED_LIBS": 0, "XXH_NO_LONG_LONG": 1})
-                cmake.configure(source_dir="cmake_unofficial", defs=cmake_defines)
-        
-            if self.should_build:
-                cmake.build()
-            if self.should_install:
-                cmake.install()
-
-    def package(self):
-        self.copy("xxhash.h")
-        self.copy("*.a", src="lib", keep_path=False)
-        self.copy("*.so", src=os.path.join(self.build_folder, "lib"), keep_path=False)
-        self.copy("*.lib", keep_path=False)
+        self.python_requires["conan-helper"].module.ArchiveHelper.build(self)        
 
     def package_info(self):
-        self.cpp_info.includedirs = [""]
-        
-        self.cpp_info.libdirs = [""]
-        
-        if self.settings.compiler != "ses_arm":
-            self.cpp_info.libs = [("lib%s.a" % self.name),]
+        self._set_compile_defines()
+
+        self.python_requires["conan-helper"].module.ArchiveHelper.package_info(self)
