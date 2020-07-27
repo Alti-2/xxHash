39f41a79cfcbde9c4f59616a6c4689f2d8d0343d
Jeremy D. Gola <jeremy@alti-2.com>
2020-07-27 14:52:05 -0400

    Update conanfile.py
    Updated to latest conan helper standard.

--- conanfile.py
+++ conanfile.py
@@ -14,10 +14,8 @@ class xxHash(ConanFile):
                "bundled_mode": [True, False]}
     default_options = {"long_long_support": False, "inline_all": False, "shared": False, 
                        "bundled_mode": True}
-    generators = "cmake", "compiler_args", "visual_studio"
-    build_requires = "ninja/[>=1.9.0]", "cmake/[>=3.1.2]", "unity/[>=2.5.0]",
-    python_requires = "SES-ARM/[>=4]", "conan-helper/[>=0.0.1]",
-    python_requires_extend = "conan-helper.Importer", "conan-helper.DependencyManager", "conan-helper.ConfigurationManager", "conan-helper.ArchiveHelper"
+    python_requires = "SES-ARM/[>=4]", "conan-helper/[0.0.12]",
+    python_requires_extend = "conan-helper.ArchiveHelper"
     _source_list = ["xxhash.c"]
     _header_list = ["xxhash.h"]
     _compile_defines = {"XXH_BUNDLED_MODE": None}
@@ -31,13 +29,3 @@ class xxHash(ConanFile):
             self._compile_defines["XXH_NO_LONG_LONG"] = 1
 
         self._compile_defines["XXH_BUNDLED_MODE"] = int(self.options.bundled_mode == True)
-
-    def build(self):
-        self._set_compile_defines()
-
-        self.python_requires["conan-helper"].module.ArchiveHelper.build(self)        
-
-    def package_info(self):
-        self._set_compile_defines()
-
-        self.python_requires["conan-helper"].module.ArchiveHelper.package_info(self)
